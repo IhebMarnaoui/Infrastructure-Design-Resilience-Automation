b66a3e733c4baf9d44a4c63d33c5795c1e07d758
Iheb Marnaoui <ihebmar@Ihebs-MacBook-Pro.local>
2025-05-28 00:41:00 +0100

    fix

--- failover/failover_webhook.py
+++ failover/failover_webhook.py
@@ -1,32 +1,27 @@
+import logging
 from flask import Flask, request
 import subprocess
-import logging
 
 app = Flask(__name__)
-
-logging.basicConfig(filename='/home/ubuntu/failover/webhook.log', level=logging.INFO)
+logging.basicConfig(filename='/home/ubuntu/Infrastructure-Design-Resilience-Automation/failover/webhook.log', level=logging.INFO)
 
 @app.route('/failover', methods=['POST'])
 def failover():
-    try:
-        data = request.json
-        app.logger.info(f"Received alert: {data}")
-
-        alerts = data.get('alerts', [])
-        for alert in alerts:
-            labels = alert.get('labels', {})
-            alertname = labels.get('alertname')
-
+    data = request.json
+    logging.info("Received alert: %s", data)
+    alerts = data.get('alerts', [])
+    for alert in alerts:
+        alertname = alert['labels'].get('alertname')
+        try:
             if alertname == "OnPremDown":
-                subprocess.call(["/home/ubuntu/failover/start_cloud.sh"])
+                logging.info("Executing start_cloud.sh")
+                subprocess.call(["/home/ubuntu/Infrastructure-Design-Resilience-Automation/failover/start_cloud.sh"])
             elif alertname == "OnPremUp":
-                subprocess.call(["/home/ubuntu/failover/stop_cloud.sh"])
-
-        return "OK", 200
-
-    except Exception as e:
-        app.logger.error(f"Error handling alert: {e}")
-        return "Internal Server Error", 500
+                logging.info("Executing stop_cloud.sh")
+                subprocess.call(["/home/ubuntu/Infrastructure-Design-Resilience-Automation/failover/stop_cloud.sh"])
+        except Exception as e:
+            logging.error("Error handling alert: %s", str(e))
+    return "OK", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6000)
